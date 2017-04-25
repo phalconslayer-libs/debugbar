528d7993ed518805981b1e8d003016b63c3b1f01
Daison Carino <daison.carino@plus65.com.sg>
2017-04-25 01:05:53 +0000

    getting dispatcher suffixes for controller and action

--- src/app/routes.php
+++ src/app/routes.php
@@ -2,20 +2,22 @@
 
 use Clarity\Facades\Route;
 
+$dispatcher = di()->get('dispatcher');
+
 Route::add('/_debugbar/open', array(
     'namespace' => 'Snowair\Debugbar\Controllers',
-    'controller' => 'OpenHandlerController',
-    'action' => 'handleAction',
+    'controller' => sprintf('OpenHandler%s', $dispatcher->getControllerSuffix()),
+    'action' => sprintf('handle%s', $dispatcher->getActionSuffix()),
 ))->setName('debugbar.openhandler');
 
 Route::add('/_debugbar/assets/stylesheets', array(
     'namespace' => 'Snowair\Debugbar\Controllers',
-    'controller' => 'AssetController',
-    'action' => 'cssAction',
+    'controller' => sprintf('Asset%s', $dispatcher->getControllerSuffix()),
+    'action' => sprintf('css%s', $dispatcher->getActionSuffix()),
 ))->setName('debugbar.assets.css');
 
 Route::add('/_debugbar/assets/javascript', array(
     'namespace' => 'Snowair\Debugbar\Controllers',
-    'controller' => 'AssetController',
-    'action' => 'jsAction',
+    'controller' => sprintf('Asset%s', $dispatcher->getControllerSuffix()),
+    'action' => sprintf('js%s', $dispatcher->getActionSuffix()),
 ))->setName('debugbar.assets.js');
